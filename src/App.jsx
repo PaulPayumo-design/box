6e91abc1ec0e2780c2c3cd68de99155896e4de86
PaulPayumo-design <jpspayumo.student@ua.edu.ph>
2026-02-10 14:04:19 +0800

    Update Payumo ALL CAPS

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Header surname="Payumo" />
+      <Header surname="PAYUMO" />
       <Box subjectCode="C-PEITEL3" />
     </div>
   );
